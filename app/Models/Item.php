7431cc1c2afb5c5604114d0717e1ce9a09265713
Marci <sos.m4rci@gmail.com>
2023-11-25 21:09:55 +0100

    extend

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
 }
