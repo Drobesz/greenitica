896a30435f6e09c7d1b7e9d44932725fcd39046c
Marci <sos.m4rci@gmail.com>
2023-11-26 00:50:00 +0100

    remove angular

--- public/greenitica-frontend/src/app/lib/interfaces/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user.interface';
-export * from './partner.interface';
-export * from './item.interface';
-export * from './transaction.interface';
-export * from './user_item.interface';
--- public/greenitica-frontend/src/app/lib/interfaces/partner.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Partner {
-    id: number,
-    name: string,
-    is_elevated: boolean,
-    percentage: number,
-    image: string
-}
