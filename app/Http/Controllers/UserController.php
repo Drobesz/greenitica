f2e11c43fff04bf879f88468401f021f2bb8636d
Marci <sos.m4rci@gmail.com>
2023-11-25 21:59:59 +0100

    extend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('profile', User::findOrFail(1));
+        return view('profile', ['user' => User::findOrFail(1)]);
     }
 
     /**
