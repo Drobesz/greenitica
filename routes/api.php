5e3e2a9a2eac79e4bd6288daa2d89a4507b60d89
Marci <sos.m4rci@gmail.com>
2023-11-25 12:26:05 +0100

    initial commit - laravel

---
+++ routes/api.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('partners', function() {
+    return
+        [
+            [
+                'id' => 1,
+                'name' => 'VKB',
+                'is_elevated' => true,
+                'percentage' => 0.06,
+                'image' => ''
+            ],
+            [
+                'id' => 2,
+                'name' => 'SISOA',
+                'is_elevated' => true,
+                'percentage' => 0.05,
+                'image' => ''
+            ],
+            [
+                'id' => 3,
+                'name' => 'RAPS',
+                'is_elevated' => false,
+                'percentage' => 0.04,
+                'image' => ''
+            ],
+            [
+                'id' => 3,
+                'name' => 'ABC',
+                'is_elevated' => false,
+                'percentage' => 0.04,
+                'image' => ''
+            ],
+        ];
+});
+
+Route::get('users', function() {
+    return
+        [
+            [
+                'id' => 1,
+                'name' => 'Kovács József',
+                'email' => 'kovacs.jozsef@greenitica.local',
+                'balance' => 100,
+            ],
+        ];
+});
+Route::get('transactions', function() {
+    return
+        [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'partner_id' => 1,
+                'amount' => 30000,
+                'category' => 'Transportation',
+                'date' => Date::Now(),
+                'carbon_token' => 1500,
+            ],
+        ];
+});
+
+
