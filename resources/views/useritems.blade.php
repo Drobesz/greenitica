615cb8131918410a7b384236999ac9b47a9ff2f8
Marci <sos.m4rci@gmail.com>
2023-11-26 02:42:03 +0100

    fix bug

--- resources/views/useritems.blade.php
+++ resources/views/useritems.blade.php
@@ -47,9 +47,9 @@
             <div class="header">Here is your code!</div>
             <div class="content">
                 Code: <b>{{$userItem->qr_code}}</b>
-                <div id="qrcode"></div>
+                <div id="qrcod{{$userItem->id}}e"></div>
                 <script type="text/javascript">
-                    new QRCode(document.getElementById("qrcode"), "{{$userItem->qr_code}}");
+                    new QRCode(document.getElementById("qrcode{{$userItem->id}}"), "{{$userItem->qr_code}}");
                 </script>
             </div>
             <div class="actions">
